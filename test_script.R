9218a9ad1680b778801be62f95b8fc326ac321f3
cmacche <99197034+cmacche@users.noreply.github.com>
2022-02-07 11:48:27 -0500

    test
    test script

---
+++ test_script.R
@@ -0,0 +1,13 @@
+
+library(usethis)
+use_git_config(user.name = "cmacche", user.email = "cmaccheyne@gmail.com")
+
+
+## produce 100 random numbers that follows a normal distribution
+x <- rnorm(100, mean = 0, sd = 1)
+
+## estimate mean
+mean(x)
+
+## estimate SD
+sd(x
